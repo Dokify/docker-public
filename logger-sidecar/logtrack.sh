65f0ca2d02eeceea6722ddbd4ae736bb978cda76
Rafael Calleja <rafaelcalleja@gmail.com>
2021-02-02 14:33:47 +0100

    create image

---
+++ logger-sidecar/logtrack.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+trap cleanup INT
+
+function cleanup() {
+  while read pid; do kill $pid; echo killed $pid; done < /tmp/pids
+}
+
+: > /tmp/pids
+
+for log in $(find /logs -type f); do
+  addTail n logs $log
+done
+
+inotifywait -q -r -m "/logs" -e create -e moved_to |
+    while read dir action file; do
+        filepath=$(readlink -fn $dir$file)
+        echo "The file '$file' appeared in directory '$dir' via '$action'"
+        if [[ -f "$filepath" ]]
+        then
+            addTail n logs $filepath
+            echo "$filepath: $(cat $filepath)"
+        fi
+    done
+
+inotifyd addTail /logs:n
